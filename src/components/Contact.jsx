3cadfc002ec065179c8eb12bda7bc0866ddeb5f1
Omar Bassatni <omarbassatni@gmail.com>
2022-12-16 00:20:59 +0200

    validation for form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,9 +8,9 @@ const Contact = () => {
                     <p className='text-4xl font-bold inline border-b-4 border-secondary text-gray-300'>Contact</p>
                     <p className='text-gray-300 py-4'> Submit the form below or shoot me an email</p>
                 </div>
-                <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
-                <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
-                <textarea className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message'></textarea>
+                <input required className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
+                <input required className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder='Email' name='email' />
+                <textarea required className='bg-[#ccd6f6] p-2' name="message" rows="10" placeholder='Message'></textarea>
                 <button className='text-white border-2 hover:bg-secondary hover:border-secondary px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
             </form>
         </div>
