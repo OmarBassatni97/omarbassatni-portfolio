f0a6e1a9952b7b790387adabdd4fac1b8eda1f24
Omar Bassatni <omarbassatni@gmail.com>
2022-12-15 15:42:19 +0200

    finished navbar

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <section className='w-full h-screen bg-primary'>
+
+        </section>
+    )
+}
+
+export default Home
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#21272f',
+        secondary: '#00bd95'
+      }
+    },
   },
   plugins: [],
 }
