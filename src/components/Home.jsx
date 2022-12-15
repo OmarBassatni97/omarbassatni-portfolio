768eae46b5f4f27f92f852e627f8c402713b4231
Omar Bassatni <omarbassatni@gmail.com>
2022-12-15 15:48:22 +0200

    finished home section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { HiArrowNarrowRight } from 'react-icons/hi'
 
 const Home = () => {
     return (
-        <section className='w-full h-screen bg-primary'>
-
+        <section name='home' className='w-full h-screen bg-primary'>
+            <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+                <p className='text-secondary'>Hi, my name is</p>
+                <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
+                    OMAR BASSATNI
+                </h1>
+                <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
+                    I'm a Front End Developer.
+                </h2>
+                <p className='text-[#8892b0] py-4 max-w-[700px]'>
+                    I’m a front end developer specializing in building (and occasionally
+                    designing) exceptional digital experiences. Currently, I’m focused on
+                    building responsive web applications.
+                </p>
+                <div>
+                    <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-secondary hover:border-secondary'>
+                        View Work
+                        <span className='group-hover:rotate-90 duration-300'>
+                            <HiArrowNarrowRight className='ml-3 ' />
+                        </span>
+                    </button>
+                </div>
+            </div>
         </section>
     )
 }
