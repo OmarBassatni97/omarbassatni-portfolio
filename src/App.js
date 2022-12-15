72fd276e7d3546dbf38f1fd403a7b5b68913c57f
Omar Bassatni <omarbassatni@gmail.com>
2022-12-15 18:18:00 +0200

    deleted comments

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import { Helmet } from "react-helmet";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Home from "./components/Home";
@@ -9,11 +8,6 @@ import Work from "./components/Work";
 function App() {
   return (
     <div className="App">
-      {/* <Helmet>
-        <meta charSet="utf-8" />
-        <title>OmarBassatni</title>
-        <link rel="canonical" href="http://mysite.com/example" />
-      </Helmet> */}
       <Navbar />
       <Home />
       <About />
