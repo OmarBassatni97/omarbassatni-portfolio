cc52d849e0830dc446ce55a676fa0f90650bdc44
Omar Bassatni <omarbassatni@gmail.com>
2022-12-20 14:26:21 +0200

    Added writing animation in home Screen

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import { HiArrowNarrowRight } from 'react-icons/hi'
 import { Link } from 'react-scroll'
+import { useTypewriter, Cursor } from 'react-simple-typewriter'
 
 const Home = () => {
+    const [text] = useTypewriter({
+        words: [' OMAR BASSATNI', ' I am a Front End Developer.'],
+        loop: true,
+        delaySpeed: 1500
+    })
     return (
         <section name='home' className='w-full h-screen bg-primary'>
             <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
                 <p className='text-secondary'>Hi, my name is</p>
                 <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
-                    OMAR BASSATNI
+                    <span>{text}</span>
+                    <Cursor cursorColor='#00bd95' />
                 </h1>
-                <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
+                {/* <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
                     I'm a Front End Developer.
-                </h2>
+                </h2> */}
                 <p className='text-[#8892b0] py-4 max-w-[700px]'>
                     I’m a front end developer specializing in building (and occasionally
                     designing) exceptional digital experiences. Currently, I’m focused on
