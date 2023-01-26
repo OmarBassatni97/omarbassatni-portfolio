4f415a0585c22ca5b37ba8d766646f7495daa143
Omar Bassatni <omarbassatni@gmail.com>
2023-01-26 15:09:38 +0200

    adding top scrollbar

--- src/App.js
+++ src/App.js
@@ -4,10 +4,23 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
 import Work from "./components/Work";
+import { motion, useScroll, useSpring } from 'framer-motion';
+
+
 
 function App() {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  });
   return (
     <div >
+      <motion.div
+        className="fixed top-0 left-0 right-0 h-[10px] bg-secondary z-[1000] origin-top-left"
+        style={{ scaleX }}
+      />
       <Navbar />
       <Home />
       <About />
